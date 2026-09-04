bca2496d6816741e77da0a6008b76a6092f69079
kj-gram <voencestra12@gmail.com>
2026-09-04 17:57:19 +0900

    fix: remove build, install, log artifacts and update gitignore

--- build/cap_robot/share/cap_robot/hook/pythonpath_develop.ps1
+++
@@ -1,3 +0,0 @@
-# generated from colcon_powershell/shell/template/hook_prepend_value.ps1.em
-
-colcon_prepend_unique_value PYTHONPATH "$env:COLCON_CURRENT_PREFIX\/home/kimhakjin/colcon_ws/src/cap_robot/build/cap_robot"
--- build/cap_robot/share/cap_robot/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/kimhakjin/colcon_ws/src/cap_robot/build/cap_robot"
